ccb5ea4c202b8c6156962220809c81d9b242ff2d
Daan Pronk <daanpronk570@gmail.com>
2026-03-27 13:03:43 +0100

    todos

--- bo-react-todo-m7/todo-bo-m7/src/addTodo/AddTodo.jsx
+++ bo-react-todo-m7/todo-bo-m7/src/addTodo/AddTodo.jsx
@@ -6,7 +6,7 @@ export function AddTodo(){
                 <h2 className='addTodo-title'>Maak een nieuwe todo</h2>
                 <div className="addTodo-card">
                     <form className="addTodo-form" action="" method="post">
-                        <input className='inputAddTodo' type="text" name="inputAddTodo" id="inputAddTodo" />
+                        <input className='inputAddTodo' type="text" name="inputAddTodo" id="inputAddTodo" placeholder='Schrijf hier een nieuwe todo'/>
                         <br />
                         <input type="submit" value="Add Todo" />
                     </form>
