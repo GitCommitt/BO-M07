76e8ba08eb7de3779d353a33b92c095e265b81cc
Sem Pater <38252@ma-web.nl>
2026-03-27 12:33:19 +0100

    footer

--- bo-react-todo-m7/todo-bo-m7/src/app/App.jsx
+++ bo-react-todo-m7/todo-bo-m7/src/app/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Navbar } from '../navbar/Navbar'
 import { AddTodo} from '../addTodo/AddTodo'
+import { Footer } from '../footer/Footer'
 
 function App() {
   return (
     <main>
       <Navbar/>
-      <AddTodo/>   
+      <AddTodo/>
+      <Footer/>   
     </main>
   )
 }
---
+++ bo-react-todo-m7/todo-bo-m7/src/footer/footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export function Footer() {
+    return (
+        <footer class="footer">
+        <div class="footer-content">
+            <p>&copy; 2026 Todos. Alle rechten voorbehouden.</p>
+            <ul class="footer-links">
+            <li><a href="#">Privacybeleid</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        </footer>
+    )
+}
